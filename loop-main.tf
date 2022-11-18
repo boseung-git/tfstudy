d335da8807c9eb29dbb6da3c358c2a5503dff313
boseung-git <117445251+boseung-git@users.noreply.github.com>
2022-11-18 10:28:00 +0900

    Add files via upload

---
+++ loop-main.tf
@@ -0,0 +1,32 @@
+
+locals {
+  nickname = "boseung-study"
+}
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "boseung-ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  count = 3
+  tags = {
+    Name = "${local.nickname}-${count.index}"
+  }
+}
+
+output "ec2-create" {
+  value = ["${aws_instance.boseung-ec2.*.tags.Name}"]
+}
